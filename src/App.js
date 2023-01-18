b64d46b1ced2d0f55251e4c14eba6920a843282b
YurtaevAndreyA <YurtaevAndreyA@gmail.com>
2023-01-18 18:03:41 +0300

    lesson 11: starting to make up the network * structure of Dimych
    Dimych has such a code in the App.js

--- src/App.js
+++ src/App.js
@@ -6,47 +6,51 @@ let App = () => {
 		return (
 				<div className="app-wrapper">
 						<header className='header'>
-								<img src=''/>
+								<img
+										src='https://avatars.mds.yandex.net/i?id=51713017d31a4b9bc1e2d35585b7ee742b5126c5-7757653-images-thumbs&n=13'/>
 						</header>
-						<nav className='nav'>
-								<div>
-										<a>Profile</a>
-								</div>
-								<div>
-										<a>Messages</a>
-								</div>
-								<div>
-										<a>News</a>
-								</div>
-								<div>
-										<a>Music</a>
-								</div>
-								<div>
-										<a>Settings</a>
-								</div>
-						</nav>
-						<div className='content'>
-								<div>
-										<img src=''/>
-								</div>
-								<div>
-										ava + description
-								</div>
-								<div>
-										My posts
+						<main>
+								<nav className='nav'>
 										<div>
-												New post
+												<a>Profile</a>
 										</div>
 										<div>
+												<a>Messages</a>
+										</div>
+										<div>
+												<a>News</a>
+										</div>
+										<div>
+												<a>Music</a>
+										</div>
+										<div>
+												<a>Settings</a>
+										</div>
+								</nav>
+								<div className='content'>
+										<div>
+												<img
+														src='https://avatars.mds.yandex.net/i?id=b4cfb9f93cd8305a618b6058056eea509961b5c7-8196573-images-thumbs&n=13'/>
+										</div>
+										<div>
+												ava + description
+										</div>
+										<div>
+												My posts
 												<div>
-														post 1
+														New post
 												</div>
 												<div>
-														post 2
+														<div>
+																post 1
+														</div>
+														<div>
+																post 2
+														</div>
 												</div>
 										</div>
 								</div>
-						</div>
+						</main>
 				</div>
 		);
 }
