0b2d460b63b4209f68790b8d730592c5118c32e3
YurtaevAndreyA <YurtaevAndreyA@gmail.com>
2023-01-18 16:38:02 +0300

    Dimych has such a code in the App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
 // import logo from './logo.svg';
 import './App.css';
-import Lalala from './Header';
-import Cococo from './Technologies';
+
 
 let App = () => {
 		return (
-				<div className="App">
-						<Lalala />
-						<Cococo />
+				<div className="app-wrapper">
+						<header className='header'>
+								<img src=''/>
+						</header>
+						<nav className='nav'>
+								<div>
+										<a>Profile</a>
+								</div>
+								<div>
+										<a>Messages</a>
+								</div>
+								<div>
+										<a>News</a>
+								</div>
+								<div>
+										<a>Music</a>
+								</div>
+								<div>
+										<a>Settings</a>
+								</div>
+						</nav>
+						<div className='content'>
+								<div>
+										<img src=''/>
+								</div>
+								<div>
+										ava + description
+								</div>
+								<div>
+										My posts
+										<div>
+												New post
+										</div>
+										<div>
+												<div>
+														post 1
+												</div>
+												<div>
+														post 2
+												</div>
+										</div>
+								</div>
+						</div>
 				</div>
 		);
 }
+
 
 export default App;
